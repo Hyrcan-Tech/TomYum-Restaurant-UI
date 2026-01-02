8b6f304473d5255ee98099ff21d543306c6a1708
[dyad] <git@dyad.sh>
2026-01-02 12:30:48 +0330

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Sidebar } from "@/components/sidebar";
+import { RestaurantProvider } from "@/lib/restaurant-context";
 import Index from "./pages/Index";
 import Tasks from "./pages/Tasks";
 import Robots from "./pages/Robots";
@@ -25,29 +26,31 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <div className="flex">
-          <Sidebar />
-          <main className="flex-1 md:ml-20">
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/tasks" element={<Tasks />} />
-              <Route path="/robots" element={<Robots />} />
-              <Route path="/map" element={<MapPage />} />
-              <Route path="/analytics" element={<Analytics />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/charging" element={<Charging />} />
-              <Route path="/menu" element={<Menu />} />
-              <Route path="/customers" element={<Customers />} />
-              <Route path="/payments" element={<Payments />} />
-              <Route path="/inventory" element={<Inventory />} />
-              <Route path="/customer-order" element={<CustomerOrder />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-        </div>
-      </BrowserRouter>
+      <RestaurantProvider>
+        <BrowserRouter>
+          <div className="flex">
+            <Sidebar />
+            <main className="flex-1 md:ml-20">
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/tasks" element={<Tasks />} />
+                <Route path="/robots" element={<Robots />} />
+                <Route path="/map" element={<MapPage />} />
+                <Route path="/analytics" element={<Analytics />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/charging" element={<Charging />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/customers" element={<Customers />} />
+                <Route path="/payments" element={<Payments />} />
+                <Route path="/inventory" element={<Inventory />} />
+                <Route path="/customer-order" element={<CustomerOrder />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+          </div>
+        </BrowserRouter>
+      </RestaurantProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
